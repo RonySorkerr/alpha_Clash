31aca95d8081600261db5ed7566a1a5e36d31971
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-15 19:18:23 +0600

    not getting anything

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,15 @@
 function play(){
+    // it will hide the screen
     hideElementById('home_screen')
+    // it will show the next screen
     showElementById('playground')
+    // random alphabet
+    continueGame();
+
      
+}
+
+function continueGame (){
+    const getRandomAlphabet = randomAlphabet();
+
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,9 +1,36 @@
-function hideElementById(elementId){
+// it will hide the screen
+function hideElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('hidden')
 }
 
-function showElementById(elementId){
+// it will show the next screen
+function showElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden')
 };
+
+
+
+
+// the alphabets array
+const randomAlphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// this will generet random aplhabet
+
+function randomAlphabet() {
+    // there is 26 alphabets we have to generat random number from 0 - 25. cause, length is array.length - 1;
+    const randomNuber = Math.random() * 25;
+
+    // convert the floor number into integer
+    const indexOfNumbers = Math.round(randomNuber);
+
+    // it generates a random number and from the number of index get an alphabet.
+
+    const alphabet = randomAlphabet(indexOfNumbers);
+
+    console.log(alphabet);
+
+    // return alphabet;
+}
+
